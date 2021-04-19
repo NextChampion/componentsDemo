fd28a3a3dae89cf49327583ce8ae35955dc9beca
zhangcunxia <252946169@qq.com>
2021-04-19 18:05:02 +0800

    feat: 添加组件列表

---
+++ src/Screens/HomeScreen/LocalData.js
@@ -0,0 +1,64 @@
+const ComponentsList = [
+  {
+    id: '111',
+    name: 'View',
+  },
+  {
+    id: '112',
+    name: 'Text',
+  },
+  {
+    id: '113',
+    name: 'Image',
+  },
+  {
+    id: '114',
+    name: 'TextInput',
+  },
+  {
+    id: '115',
+    name: 'ScrollView',
+  },
+  {
+    id: '116',
+    name: 'Button',
+  },
+  {
+    id: '117',
+    name: 'Switch',
+  },
+  {
+    id: '118',
+    name: 'FlatList',
+  },
+  {
+    id: '119',
+    name: 'SectionList',
+  },
+  {
+    id: '120',
+    name: 'ActivityIndicator',
+  },
+  {
+    id: '121',
+    name: 'Alert',
+  },
+  {
+    id: '122',
+    name: 'KeyboardAvoidingView',
+  },
+  {
+    id: '123',
+    name: 'Modal',
+  },
+  {
+    id: '124',
+    name: 'RefreshControl',
+  },
+  {
+    id: '125',
+    name: 'StatusBar',
+  },
+];
+
+export default ComponentsList;
--- src/Screens/HomeScreen/index.js
+++ src/Screens/HomeScreen/index.js
@@ -1,18 +1,54 @@
 import React, {PureComponent} from 'react';
-import {Text, View, Button} from 'react-native';
+import {Text, View, Button, FlatList, StyleSheet} from 'react-native';
+import ComponentsList from './LocalData';
 
 export default class HomeScreen extends PureComponent {
-  onButtonPress = () => {
+  onButtonPress = name => {
     const {navigation} = this.props;
-    navigation.navigate('View');
-    console.log('onButtonPress', this.props);
+    navigation.navigate(name);
   };
+
+  renderItem = ({item}) => {
+    const {name} = item || {};
+    return (
+      <View style={styles.row}>
+        <Text>组件名称:</Text>
+        <Button title={name} onPress={() => this.onButtonPress(name)} />
+      </View>
+    );
+  };
+
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
-        <Button title="View" onPress={this.onButtonPress} />
+      <View style={styles.container}>
+        <View style={styles.titleView}>
+          <Text style={styles.title}> 组件列表: </Text>
+        </View>
+        <FlatList data={ComponentsList} renderItem={this.renderItem} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  titleView: {
+    marginVertical: 5,
+    marginLeft: 5,
+    borderBottomWidth: 1,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  row: {
+    paddingHorizontal: 19,
+    flexDirection: 'row',
+    paddingVertical: 8,
+    marginBottom: 3,
+    borderBottomWidth: 1,
+    alignItems: 'center',
+  },
+});
