f8eb627256a87af082584b6d57830db5d8846a30
zhangcunxia <252946169@qq.com>
2021-04-22 20:57:51 +0800

    feat: 添加输入框例子

--- src/Screens/TextInputScreen/index.js
+++ src/Screens/TextInputScreen/index.js
@@ -1,12 +1,30 @@
 import React, {PureComponent} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TextInput, StyleSheet} from 'react-native';
 
 export default class TextInputScreen extends PureComponent {
   render() {
     return (
-      <View>
-        <Text> TextInputScreen </Text>
+      <View style={styles.contaier}>
+        <Text>普通输入框, Android有固定的高度</Text>
+        <TextInput placeholder="TextInput" style={styles.textInput} />
+        <Text style={styles.text}>双平台一致:</Text>
+        <TextInput value="TextInput" style={styles.textInput1} />
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  contaier: {
+    flex: 1,
+  },
+  textInput: {
+    backgroundColor: 'red',
+  },
+  text: {
+    marginTop: 10,
+  },
+  textInput1: {
+    backgroundColor: 'red',
+    height: 50,
+  },
+});
