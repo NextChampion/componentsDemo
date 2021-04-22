e2491292924d39345be7502afc3c504ebca19774
zhangcunxia <252946169@qq.com>
2021-04-22 21:12:52 +0800

    feat: 滚动例子

--- src/Screens/ScrollViewScreen/index.js
+++ src/Screens/ScrollViewScreen/index.js
@@ -1,12 +1,62 @@
 import React, {PureComponent} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet, ScrollView, RefreshControl} from 'react-native';
+
+const wait = timeout => {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeout);
+  });
+};
 
 export default class ScrollViewScreen extends PureComponent {
+  state = {
+    refreshing: false,
+  };
+
+  onRefresh = () => {
+    this.setState({refreshing: true});
+    wait(2000).then(() => this.setState({refreshing: false}));
+  };
   render() {
+    const {refreshing} = this.state;
     return (
-      <View>
-        <Text> ScrollViewScreen </Text>
+      <View style={styles.container}>
+        {/* <Text> ScrollViewScreen </Text> */}
+        <ScrollView
+          style={styles.scrollView}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={this.onRefresh}
+            />
+          }>
+          <Text>ScrollView</Text>
+        </ScrollView>
+        <View style={styles.scrollContainer}>
+          <ScrollView style={styles.scrollView1}>
+            <Text>ScrollView</Text>
+          </ScrollView>
+        </View>
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingBottom: 200,
+  },
+  scrollView: {
+    backgroundColor: 'red',
+    height: 100,
+    width: 300,
+  },
+  scrollContainer: {
+    height: 100,
+    width: 300,
+  },
+  scrollView1: {
+    backgroundColor: 'green',
+    height: 100,
+    width: 300,
+  },
+});
